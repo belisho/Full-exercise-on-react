8a337c24f3a5b20b3ca5df88d8bd4db12828372e
belisho <belete.a1627@gmail.com>
2024-02-27 12:32:39 +0300

    add pages folder and delete app.js file

---
+++ src/pages/firstPage.js
@@ -0,0 +1,13 @@
+
+function FirstPage() {
+    return(
+        <div>
+            <header className="App-header">
+                <h1>Exercise one</h1>
+            </header>
+            <h1>exercise one body</h1>
+         </div>
+    );
+}
+
+export default FirstPage;
